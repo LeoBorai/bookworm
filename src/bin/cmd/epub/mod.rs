87515a3141cd30d2a44aebbb1a825f7f0e16c7c3
Leo Borai <estebanborai@gmail.com>
2026-01-02 00:05:53 +0100

    feat: introduce `unpkg` subcommand

--- src/bin/cmd/epub/mod.rs
+++ src/bin/cmd/epub/mod.rs
@@ -1,19 +1,24 @@
 mod info;
+mod unpackage;
 
 use clap::Subcommand;
 
 use self::info::InfoOpt;
+use self::unpackage::UnPackageOpt;
 
 #[derive(Clone, Debug, Subcommand)]
 pub enum EpubCmd {
     /// Retrieve (K)Epub File Information
     Info(InfoOpt),
+    /// Unpackage (K)Epub File
+    Unpkg(UnPackageOpt),
 }
 
 impl EpubCmd {
     pub async fn exec(&self) -> anyhow::Result<()> {
         match self {
             Self::Info(cmd) => cmd.exec().await,
+            Self::Unpkg(cmd) => cmd.exec().await,
         }
     }
 }
