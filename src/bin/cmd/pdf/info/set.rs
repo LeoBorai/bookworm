fccd181d8b3758b4b7722dce132d14e538c01807
Leo Borai <estebanborai@gmail.com>
2026-01-03 21:35:41 +0100

    feat: support to set many fiel values

--- src/bin/cmd/pdf/info/set.rs
+++ src/bin/cmd/pdf/info/set.rs
@@ -3,44 +3,62 @@ use std::path::PathBuf;
 use anyhow::Result;
 use clap::Args;
 
-use bookworm::pdf::Pdf;
+use bookworm::pdf::{Pdf, PdfMetaField};
 
 #[derive(Args, Clone, Debug)]
 pub struct SetOpt {
     /// Path to the PDF file
     path: PathBuf,
+    /// Sets the `Title` field
+    #[clap(long)]
+    title: Option<String>,
+    /// Sets the `Author` field
+    #[clap(long)]
+    author: Option<String>,
+    /// Sets the `Creator` field
+    #[clap(long)]
+    creator: Option<String>,
+    /// Sets the `Producer` field
+    #[clap(long)]
+    producer: Option<String>,
+    /// Sets the `Creation Date` field
+    #[clap(long)]
+    creation_date: Option<String>,
+    /// Sets the `Modification Date` field
+    #[clap(long)]
+    modification_date: Option<String>,
 }
 
 impl SetOpt {
     pub async fn exec(&self) -> Result<()> {
-        let pdf = Pdf::open(&self.path)?;
-        let info = pdf.metadata()?;
+        let mut pdf = Pdf::open(&self.path)?;
 
-        println!(
-            "Title: {}",
-            info.title.unwrap_or_else(|| "Unknown".to_string())
-        );
-        println!(
-            "Author: {}",
-            info.author.unwrap_or_else(|| "Unknown".to_string())
-        );
-        println!(
-            "Creator: {}",
-            info.creator.unwrap_or_else(|| "Unknown".to_string())
-        );
-        println!(
-            "Producer: {}",
-            info.producer.unwrap_or_else(|| "Unknown".to_string())
-        );
-        println!(
-            "Creation Date: {}",
-            info.creation_date.unwrap_or_else(|| "Unknown".to_string())
-        );
-        println!(
-            "Modification Date: {}",
-            info.modification_date
-                .unwrap_or_else(|| "Unknown".to_string())
-        );
+        if let Some(title) = &self.title {
+            pdf.set_metadata(&PdfMetaField::Title, title)?;
+        }
+
+        if let Some(author) = &self.author {
+            pdf.set_metadata(&PdfMetaField::Author, author)?;
+        }
+
+        if let Some(creator) = &self.creator {
+            pdf.set_metadata(&PdfMetaField::Creator, creator)?;
+        }
+
+        if let Some(producer) = &self.producer {
+            pdf.set_metadata(&PdfMetaField::Producer, producer)?;
+        }
+
+        if let Some(creation_date) = &self.creation_date {
+            pdf.set_metadata(&PdfMetaField::CreationDate, creation_date)?;
+        }
+
+        if let Some(modification_date) = &self.modification_date {
+            pdf.set_metadata(&PdfMetaField::ModificationDate, modification_date)?;
+        }
+
+        let next_path = self.path.with_file_name("set_info.pdf");
+        pdf.save(next_path)?;
 
         Ok(())
     }
--- src/pdf/mod.rs
+++ src/pdf/mod.rs
@@ -1,6 +1,9 @@
-use std::path::Path;
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 use lopdf::{Dictionary, Document, Object};
 
 const PDF_META_INFO_KEY: &[u8] = b"Info";
@@ -10,6 +13,45 @@ const PDF_META_CREATOR_KEY: &[u8] = b"Creator";
 const PDF_META_PRODUCER_KEY: &[u8] = b"Producer";
 const PDF_META_CREATION_DATE_KEY: &[u8] = b"CreationDate";
 const PDF_META_MODIFICATION_DATE_KEY: &[u8] = b"ModDate";
+
+#[derive(Debug, Clone, Copy)]
+pub enum PdfMetaField {
+    Title,
+    Author,
+    Creator,
+    Producer,
+    CreationDate,
+    ModificationDate,
+}
+
+impl FromStr for PdfMetaField {
+    type Err = ();
+
+    fn from_str(input: &str) -> std::result::Result<PdfMetaField, Self::Err> {
+        match input {
+            "Title" => Ok(PdfMetaField::Title),
+            "Author" => Ok(PdfMetaField::Author),
+            "Creator" => Ok(PdfMetaField::Creator),
+            "Producer" => Ok(PdfMetaField::Producer),
+            "CreationDate" => Ok(PdfMetaField::CreationDate),
+            "ModificationDate" => Ok(PdfMetaField::ModificationDate),
+            _ => Err(()),
+        }
+    }
+}
+
+impl PdfMetaField {
+    pub fn as_bytes(&self) -> &[u8] {
+        match self {
+            PdfMetaField::Title => PDF_META_TITLE_KEY,
+            PdfMetaField::Author => PDF_META_AUTHOR_KEY,
+            PdfMetaField::Creator => PDF_META_CREATOR_KEY,
+            PdfMetaField::Producer => PDF_META_PRODUCER_KEY,
+            PdfMetaField::CreationDate => PDF_META_CREATION_DATE_KEY,
+            PdfMetaField::ModificationDate => PDF_META_MODIFICATION_DATE_KEY,
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct PdfMetadata {
@@ -24,26 +66,30 @@ pub struct PdfMetadata {
 #[derive(Debug)]
 pub struct Pdf {
     doc: Document,
+    path: PathBuf,
 }
 
 impl Pdf {
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let doc = Document::load(path)?;
-        Ok(Pdf { doc })
+        let doc = Document::load(&path)?;
+        Ok(Pdf {
+            doc,
+            path: path.as_ref().to_path_buf(),
+        })
     }
 
     pub fn metadata(&self) -> Result<PdfMetadata> {
         Ok(PdfMetadata {
-            title: self.get_metadata_field(PDF_META_TITLE_KEY),
-            author: self.get_metadata_field(PDF_META_AUTHOR_KEY),
-            creator: self.get_metadata_field(PDF_META_CREATOR_KEY),
-            producer: self.get_metadata_field(PDF_META_PRODUCER_KEY),
-            creation_date: self.get_metadata_field(PDF_META_CREATION_DATE_KEY),
-            modification_date: self.get_metadata_field(PDF_META_MODIFICATION_DATE_KEY),
+            title: self.get_metadata_field(&PdfMetaField::Title),
+            author: self.get_metadata_field(&PdfMetaField::Author),
+            creator: self.get_metadata_field(&PdfMetaField::Creator),
+            producer: self.get_metadata_field(&PdfMetaField::Producer),
+            creation_date: self.get_metadata_field(&PdfMetaField::CreationDate),
+            modification_date: self.get_metadata_field(&PdfMetaField::ModificationDate),
         })
     }
 
-    fn get_metadata_field(&self, field: &[u8]) -> Option<String> {
+    fn get_metadata_field(&self, field: &PdfMetaField) -> Option<String> {
         let doc = &self.doc;
         let info_ref = doc.trailer.get(PDF_META_INFO_KEY).ok()?;
         let object_id = match info_ref {
@@ -58,38 +104,45 @@ impl Pdf {
         let info_obj = doc.get_object(object_id).ok()?;
         let dict = info_obj.as_dict().ok()?;
 
-        dict.get(field)
+        dict.get(field.as_bytes())
             .ok()
             .and_then(|value| value.as_str().ok())
             .map(|bytes| String::from_utf8_lossy(bytes).to_string())
     }
 
-    pub fn set_metadata(&self, key: &str, value: &str) -> Result<()> {
-        let mut doc = self.doc.clone();
-        let info_id = if let Some(info_ref) = doc.trailer.get(PDF_META_INFO_KEY).ok() {
+    pub fn set_metadata(&mut self, field: &PdfMetaField, value: &str) -> Result<()> {
+        let info_id = if let Ok(info_ref) = self.doc.trailer.get(PDF_META_INFO_KEY) {
             match info_ref {
                 Object::Reference(id) => *id,
-                _ => Self::create_info_dictionary(&mut doc)?,
+                _ => Self::create_info_dictionary(&mut self.doc)?,
             }
         } else {
-            Self::create_info_dictionary(&mut doc)?
+            Self::create_info_dictionary(&mut self.doc)?
         };
 
-        if let Ok(info_obj) = doc.get_object_mut(info_id) {
-            if let Ok(dict) = info_obj.as_dict_mut() {
-                dict.set(key, Object::string_literal(value));
-            }
+        if let Ok(info_obj) = self.doc.get_object_mut(info_id)
+            && let Ok(dict) = info_obj.as_dict_mut()
+        {
+            dict.set(field.as_bytes(), Object::string_literal(value));
         }
 
-        doc.save("updated.pdf")?;
+        Ok(())
+    }
 
+    pub fn save<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
+        if self.path == path.as_ref() {
+            bail!("Cannot overwrite source file");
+        }
+
+        self.doc.save(&path)?;
         Ok(())
     }
 
     fn create_info_dictionary(doc: &mut Document) -> Result<(u32, u16)> {
         let info_dict = Dictionary::new();
         let info_id = doc.add_object(Object::Dictionary(info_dict));
-        doc.trailer.set("Info", Object::Reference(info_id));
+        doc.trailer
+            .set(PDF_META_INFO_KEY, Object::Reference(info_id));
         Ok(info_id)
     }
 }
