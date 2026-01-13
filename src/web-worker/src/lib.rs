be14a2d0f8d800e630d935a4c4a86b5883032e6a
Leo Borai <estebanborai@gmail.com>
2026-01-13 23:27:09 +0100

    feat: worker poc

--- src/web-worker/src/lib.rs
+++ src/web-worker/src/lib.rs
@@ -1,6 +1,13 @@
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_futures::{JsFuture, spawn_local};
+use web_sys::File;
+
+use bookworm::epub::Epub;
+use bookworm::pdf::Pdf;
 
 #[wasm_bindgen]
 extern "C" {
@@ -24,16 +31,46 @@ pub fn worker_entry_point() {
     let closure = Closure::wrap(Box::new(move |event: web_sys::MessageEvent| {
         log("Received event");
         let data = event.data();
+        log(&format!("Processed: {:?}", data));
+        let file: File = File::from(data);
 
-        // Process data (example: compute-heavy task)
-        let result = format!("Processed: {:?}", data);
-        log(result.as_str());
+        spawn_local(async move {
+            log("Processing file in worker");
+            let filename = file.name();
+            let array_buffer_promise = file.array_buffer();
+            let bytes = read_promise_as_bytes(array_buffer_promise).await.unwrap();
+            if filename.ends_with(".pdf") {
+                let pdf = Pdf::from_bytes(&bytes).unwrap();
+                let metadata = pdf.metadata().unwrap();
+                log(&format!(
+                    "PDF Metadata - Title: {:?}, Author: {:?}, Creation Date: {:?}",
+                    metadata.title, metadata.author, metadata.creation_date
+                ));
+            }
+
+            if filename.ends_with(".epub") {
+                let _epub = Epub::from_bytes(bytes);
+            }
+        });
+
         // Send result back to main thread
-        let global = js_sys::global().unchecked_into::<web_sys::DedicatedWorkerGlobalScope>();
-        global.post_message(&JsValue::from_str(&result)).unwrap();
+        // let global = js_sys::global().unchecked_into::<web_sys::DedicatedWorkerGlobalScope>();
+        // global.post_message(&JsValue::from_str(&result)).unwrap();
     }) as Box<dyn FnMut(_)>);
 
     let global = js_sys::global().unchecked_into::<web_sys::DedicatedWorkerGlobalScope>();
     global.set_onmessage(Some(closure.as_ref().unchecked_ref()));
     closure.forget();
+}
+
+async fn read_promise_as_bytes(promise: js_sys::Promise) -> Result<Vec<u8>> {
+    let array_buffer_value = JsFuture::from(promise)
+        .await
+        .map_err(|e| anyhow::anyhow!("Failed to read file: {:?}", e))?;
+    let array_buffer = array_buffer_value
+        .dyn_into::<js_sys::ArrayBuffer>()
+        .map_err(|_| anyhow::anyhow!("Failed to cast to ArrayBuffer"))?;
+    let uint8_array = js_sys::Uint8Array::new(&array_buffer);
+
+    Ok(uint8_array.to_vec())
 }
--- src/web/src/components/atoms/file_upload_button.rs
+++ src/web/src/components/atoms/file_upload_button.rs
@@ -1,100 +1,68 @@
 use std::rc::Rc;
 
-use anyhow::{Result, anyhow};
-use leptos::task::spawn_local;
 use leptos::{html, prelude::*};
-use wasm_bindgen::{JsCast, JsValue};
-use wasm_bindgen_futures::JsFuture;
-use web_sys::{Event, File, HtmlInputElement};
-
-use bookworm::epub::Epub;
-use bookworm::pdf::Pdf;
+use wasm_bindgen::JsCast;
+use web_sys::{Event, HtmlInputElement};
 
 use crate::WEB_WORKER;
-use crate::contexts::books::Book;
 
 #[component]
 pub fn FileUploadButton(#[prop(into)] class: String, children: Children) -> impl IntoView {
     let file_input_el = NodeRef::<html::Input>::new();
     let on_file_change = move |ev: Event| {
-        // let el = ev
-        //     .target()
-        //     .expect("Failed to retrieve target")
-        //     .unchecked_into::<HtmlInputElement>();
+        let el = ev
+            .target()
+            .expect("Failed to retrieve target")
+            .unchecked_into::<HtmlInputElement>();
 
-        // if let Some(files) = el.files()
-        //     && let Some(file) = files.get(0)
-        // {
-        //     spawn_local(async move {
-        //         let filename = file.name();
-        //         let array_buffer_promise = file.array_buffer();
-        //         let bytes = read_promise_as_bytes(array_buffer_promise).await.unwrap();
-        //         if filename.ends_with(".pdf") {
-        //             let pdf = Pdf::from_bytes(&bytes).unwrap();
-        //             let metadata = pdf.metadata().unwrap();
-
-        //             let b = Book {
-        //                 id: 1,
-        //                 title: metadata.title.unwrap_or(String::from("Unknown")),
-        //                 author: metadata.author.unwrap_or(String::from("Unknown")),
-        //                 date: metadata.creation_date.unwrap_or(String::from("Unknown")),
-        //                 keywords: Vec::new(),
-        //                 format: String::from("PDF"),
-        //                 size: bytes.len() as u64,
-        //             };
-
-        //             leptos::logging::log!("test: {:?}", b);
-        //         }
-
-        //         if filename.ends_with(".epub") {
-        //             let _epub = Epub::from_bytes(bytes);
-        //         }
-        //     });
-        // }
-    };
-
-    let send_to_worker = move |_| {
-        WEB_WORKER.with(Rc::clone).borrow().as_ref().map(|worker| {
-            leptos::logging::log!("Got worker");
-            if let Err(err) = worker
-                .post_message(&JsValue::from_str("Hello Worker!")) {
+        if let Some(files) = el.files()
+            && let Some(file) = files.get(0)
+        {
+            WEB_WORKER.with(Rc::clone).borrow().as_ref().map(|worker| {
+                leptos::logging::log!("Got worker");
+                if let Err(err) = worker.post_message(&file.into()) {
                     leptos::logging::log!("Failed to send message to worker: {:?}", err);
                 }
-        });
+            });
+
+            // spawn_local(async move {
+            //     let filename = file.name();
+            //     let array_buffer_promise = file.array_buffer();
+            //     let bytes = read_promise_as_bytes(array_buffer_promise).await.unwrap();
+            //     if filename.ends_with(".pdf") {
+            //         let pdf = Pdf::from_bytes(&bytes).unwrap();
+            //         let metadata = pdf.metadata().unwrap();
+
+            //         let b = Book {
+            //             id: 1,
+            //             title: metadata.title.unwrap_or(String::from("Unknown")),
+            //             author: metadata.author.unwrap_or(String::from("Unknown")),
+            //             date: metadata.creation_date.unwrap_or(String::from("Unknown")),
+            //             keywords: Vec::new(),
+            //             format: String::from("PDF"),
+            //             size: bytes.len() as u64,
+            //         };
+
+            //         leptos::logging::log!("test: {:?}", b);
+            //     }
+
+            //     if filename.ends_with(".epub") {
+            //         let _epub = Epub::from_bytes(bytes);
+            //     }
+            // });
+        }
+    };
+
+    let handle_click = move |_| {
+        if let Some(input_el) = file_input_el.get() {
+            input_el.click();
+        }
     };
 
     view! {
-        <button class=class on:click=send_to_worker>
+        <button class=class on:click=handle_click>
             <input type="file" hidden=true node_ref=file_input_el on:change=on_file_change />
             {children()}
         </button>
     }
-}
-
-async fn read_promise_as_bytes(promise: js_sys::Promise) -> Result<Vec<u8>> {
-    let array_buffer_value = JsFuture::from(promise)
-        .await
-        .map_err(|e| anyhow::anyhow!("Failed to read file: {:?}", e))?;
-    let array_buffer = array_buffer_value
-        .dyn_into::<js_sys::ArrayBuffer>()
-        .map_err(|_| anyhow::anyhow!("Failed to cast to ArrayBuffer"))?;
-    let uint8_array = js_sys::Uint8Array::new(&array_buffer);
-
-    Ok(uint8_array.to_vec())
-}
-
-async fn read_file_as_bytes(file: File) -> Result<Vec<u8>> {
-    let array_buffer_promise = file.array_buffer();
-    let array_buffer_value = JsFuture::from(array_buffer_promise)
-        .await
-        .map_err(|e| anyhow!("Failed to read file: {:?}", e))?;
-    let array_buffer = array_buffer_value
-        .dyn_into::<js_sys::ArrayBuffer>()
-        .map_err(|_| anyhow!("Failed to cast to ArrayBuffer".to_string()))?;
-
-    // Convert ArrayBuffer to Uint8Array
-    let uint8_array = js_sys::Uint8Array::new(&array_buffer);
-
-    // Convert to Vec<u8>
-    Ok(uint8_array.to_vec())
 }
